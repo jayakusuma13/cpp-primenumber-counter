1954acdc0ccd6aa84f10a24a1da96ad618f22af8
Jaya Kusuma <kusumavictor@yahoo.co.id>
2017-03-12 20:05:04 +0700

    Update PB03-16416303-170312-02.cpp

--- PB03-16416303-170312-02.cpp
+++ PB03-16416303-170312-02.cpp
@@ -1,19 +1,11 @@
-// NIM/Nama : 16416303 / M. Qodir Ibrahim Jayakusuma
-// Nama file : PB03-16416303-170312-02.cpp
-// Tanggal : 12-03-2017
-// Deskripsi : penghitung angka prima
-
-
 #include <iostream>
 using namespace std;
-//deklarasi variabel
+
     int x,y;
 
-    //fungsi
     int prima(int x, int y){
         int i,j,a,b;
 
-        //a sebagai bilangan akhir dan b sebagai bilangan awal
         if(x>y){
             a = x;
             b = y;
@@ -22,7 +14,6 @@ using namespace std;
             b = x;
         };
 
-        //looping untuk cek bilangan prima
         for(i=b;i<=a;i++){
             for(j=2;j<i;j++){
             if(i%j == 0){
@@ -39,13 +30,11 @@ using namespace std;
 
 int main(){
 
-    //input
     cout << "input nilai A: ";
     cin >> x;
     cout << "input nilai B: ";
     cin >> y;
 
-    //penggunaan fungsi
     prima(x,y);
 
 
